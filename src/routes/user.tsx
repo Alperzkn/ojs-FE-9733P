e05c016403909e9d32cd90898342abc90294d029
Alperzkn <20475676+Alperzkn@users.noreply.github.com>
2024-10-29 14:11:47 +0300

    spinners added

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -3,6 +3,7 @@ import { Accordion, Card, ListGroup } from "react-bootstrap";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import { useLoaderData, useParams } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
 interface User {
   id: number;
@@ -62,6 +63,8 @@ export default function UserPage() {
   const [posts, setPosts] = useState([]);
   const [albums, setAlbums] = useState([]);
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>();
 
   const { user } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
   const { userId } = useParams() as unknown as LoaderParams;
@@ -69,20 +72,35 @@ export default function UserPage() {
 
   useEffect(() => {
     (async () => {
-      const postsData = await getPosts();
-      //console.log("postsData --> " + JSON.stringify(postsData));
-      const albumsData = await getAlbums();
-      const todosData = await getTodos();
+      setLoading(true);
+      try {
+        const postsData = await getPosts();
+        //console.log("postsData --> " + JSON.stringify(postsData));
+        const albumsData = await getAlbums();
+        const todosData = await getTodos();
 
-      const userAlbums = albumsData.filter((el) => el.userId == 1);
-      const userTodos = todosData.filter((el) => el.userId == 1);
-      const userPosts = postsData.filter((el) => el.userId == 1);
-      //console.log("user albums --> " + userAlbums);
-      setTodos(userTodos);
-      setAlbums(userAlbums);
-      setPosts(userPosts);
+        const userAlbums = albumsData.filter((el) => el.userId == 1);
+        const userTodos = todosData.filter((el) => el.userId == 1);
+        const userPosts = postsData.filter((el) => el.userId == 1);
+        //console.log("user albums --> " + userAlbums);
+        setTodos(userTodos);
+        setAlbums(userAlbums);
+        setPosts(userPosts);
+      } catch (e) {
+        setError(e);
+        console.log(e);
+      }
+      setLoading(false);
     })();
   }, []);
+
+  if (error) {
+    return (
+      <>
+        <h1>Couldn't get data</h1>
+      </>
+    );
+  }
 
   return (
     <>
@@ -92,111 +110,121 @@ export default function UserPage() {
             <h1 className="mb-3">User Page</h1>
             <Tabs defaultActiveKey="user" id="user-tab" className="mb-3">
               <Tab eventKey="user" title="User">
-                <Card style={{ width: "18rem" }}>
-                  <ListGroup variant="flush">
-                    <ListGroup.Item>User ID: {userId}</ListGroup.Item>
-                    <ListGroup.Item>User Name: {user.name}</ListGroup.Item>
-                    <ListGroup.Item>Username: {user.username}</ListGroup.Item>
-                    <ListGroup.Item>City: {user.address.city}</ListGroup.Item>
-                    <ListGroup.Item>
-                      Street: {user.address.street}
-                    </ListGroup.Item>
-                  </ListGroup>
-                </Card>
+                {loading && <Spinner animation="grow" />}
+                {!loading && (
+                  <Card style={{ width: "18rem" }}>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>User ID: {userId}</ListGroup.Item>
+                      <ListGroup.Item>User Name: {user.name}</ListGroup.Item>
+                      <ListGroup.Item>Username: {user.username}</ListGroup.Item>
+                      <ListGroup.Item>City: {user.address.city}</ListGroup.Item>
+                      <ListGroup.Item>
+                        Street: {user.address.street}
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+                )}
               </Tab>
               <Tab eventKey="album" title="Album">
-                {albums.map((album, key) => (
-                  <>
-                    <div className="card my-3">
-                      <div className="card-header d-flex justify-content-between">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-success disabled "
-                        >
-                          Album ID{" "}
-                          <span className="badge text-dark">{album.id}</span>
-                        </button>
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">{album.title}</h5>
-                        <div className="d-flex justify-content-end">
+                {loading && <Spinner animation="grow" />}
+                {!loading &&
+                  albums.map((album, key) => (
+                    <>
+                      <div className="card my-3">
+                        <div className="card-header d-flex justify-content-between">
                           <button
                             type="button"
-                            className="btn btn-sm btn-outline-success disabled"
+                            className="btn btn-sm btn-outline-success disabled "
                           >
-                            User ID{" "}
-                            <span className="badge text-dark ">
-                              {album.userId}
-                            </span>
+                            Album ID{" "}
+                            <span className="badge text-dark">{album.id}</span>
                           </button>
                         </div>
+                        <div className="card-body">
+                          <h5 className="card-title">{album.title}</h5>
+                          <div className="d-flex justify-content-end">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-outline-success disabled"
+                            >
+                              User ID{" "}
+                              <span className="badge text-dark ">
+                                {album.userId}
+                              </span>
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                ))}
+                    </>
+                  ))}
               </Tab>
               <Tab eventKey="todos" title="Todos">
-                {todos.map((todo, key) => (
-                  <>
-                    <div className="card my-3">
-                      <div className="card-header d-flex justify-content-between">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-success disabled "
-                        >
-                          Todo ID{" "}
-                          <span className="badge text-dark">{todo.id}</span>
-                        </button>
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">{todo.title}</h5>
-                        <div className="d-flex justify-content-end">
+                {loading && <Spinner animation="grow" />}
+
+                {!loading &&
+                  todos.map((todo, key) => (
+                    <>
+                      <div className="card my-3">
+                        <div className="card-header d-flex justify-content-between">
                           <button
                             type="button"
-                            className="btn btn-sm btn-outline-success disabled"
+                            className="btn btn-sm btn-outline-success disabled "
                           >
-                            User ID{" "}
-                            <span className="badge text-dark ">
-                              {todo.userId}
-                            </span>
+                            Todo ID{" "}
+                            <span className="badge text-dark">{todo.id}</span>
                           </button>
                         </div>
+                        <div className="card-body">
+                          <h5 className="card-title">{todo.title}</h5>
+                          <div className="d-flex justify-content-end">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-outline-success disabled"
+                            >
+                              User ID{" "}
+                              <span className="badge text-dark ">
+                                {todo.userId}
+                              </span>
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                ))}
+                    </>
+                  ))}
               </Tab>
               <Tab eventKey="posts" title="Posts">
-                {posts.map((post, key) => (
-                  <>
-                    <div className="card my-3">
-                      <div className="card-header d-flex justify-content-between">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-success disabled "
-                        >
-                          Post ID{" "}
-                          <span className="badge text-dark">{post.id}</span>
-                        </button>
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <p>{post.body}</p>
-                        <div className="d-flex justify-content-end">
+                {loading && <Spinner animation="grow" />}
+                {!loading &&
+                  posts.map((post, key) => (
+                    <>
+                      <div className="card my-3">
+                        <div className="card-header d-flex justify-content-between">
                           <button
                             type="button"
-                            className="btn btn-sm btn-outline-success disabled"
+                            className="btn btn-sm btn-outline-success disabled "
                           >
-                            User ID{" "}
-                            <span className="badge text-dark ">
-                              {post.userId}
-                            </span>
+                            Post ID{" "}
+                            <span className="badge text-dark">{post.id}</span>
                           </button>
                         </div>
+                        <div className="card-body">
+                          <h5 className="card-title">{post.title}</h5>
+                          <p>{post.body}</p>
+                          <div className="d-flex justify-content-end">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-outline-success disabled"
+                            >
+                              User ID{" "}
+                              <span className="badge text-dark ">
+                                {post.userId}
+                              </span>
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                ))}
+                    </>
+                  ))}
               </Tab>
             </Tabs>
           </div>
